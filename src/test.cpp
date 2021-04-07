38c62480c5e2ae78b73ac49d33224ab24c014cf8
Feyza Gezerer <58954826+feyzagezerer@users.noreply.github.com>
2021-04-07 03:02:19 +0300

    Update test.cpp

--- src/test.cpp
+++ src/test.cpp
@@ -1,11 +1,8 @@
 /**        
-* @file  B151210046_Odev1  
+
 * @description program kullanıcının girdiği kova sayısı kadar renkli kova oluşturup 
 * içerisine bir top koyabiliyor, kovaların ve topların yerini değiştirebiliyor, kovaları ve topları tersten yerleştirebiliyor
-* @course  1B  
-* @assignment  1   
-* @date  14.10.2019-3.11.2019 
-* @author  Feyza GEZERER feyza.gezerer@ogr.sakarya.edu.tr   
+
 */
 #include "KovaKontrol.hpp"
 
